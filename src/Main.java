066d454c611f93c0c00da956b04902cca1f7ec55
Lek-San <leksan.webdev@gmail.com>
2022-12-21 16:41:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static String nom = "Ola amigos";
+
+    public static void main(String[] args){
+        System.out.println("Welcome in Java World !!!");
+
+        int data = 10;
+        System.out.println(data);
+
+        System.out.println(nom);
+
+        boolean isOpen = true;
+        System.out.println(isOpen);
+
+        byte num = 12;
+        System.out.println(num);
+        
+        double bateau = 1.20;
+        System.out.println(bateau);
+
+        char charactaire = 5;
+        System.out.println(charactaire);
+
+
+    }
+}
